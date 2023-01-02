9eabd417ae1f79e379f9c1be94fddc204df70a74
juanchoancap <juan_ancap@protonmail.com>
2023-01-02 14:52:28 -0300

    Backend 1.0 listo

---
+++ routes/events.js
@@ -0,0 +1,45 @@
+/*
+    Event Routes
+    /api/events
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const {
+    getEventos,
+    crearEvento,
+    actualizarEvento,
+    eliminarEvento,
+} = require('../controllers/events');
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+// Todas tienen que pasar por la validacion del JWT
+router.use(validarJWT);
+
+// Obtener eventos
+router.get('/', getEventos);
+
+// Crear un nuevo evento
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos,
+    ],
+    crearEvento
+);
+
+// Actualizar evento
+router.put('/:id', actualizarEvento);
+
+// Borrar evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
